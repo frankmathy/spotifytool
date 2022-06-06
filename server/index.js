8d673f85074a7556c1877ab99791338da6f17dd1
Frank Mathy <frankmathy@gmail.com>
2022-06-06 18:00:39 +0200

    Finished base server

--- server/index.js
+++ server/index.js
@@ -19,4 +19,39 @@ app.post('/login', async (req, res) => {
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
   });
+
+  try {
+    const {
+      body: { access_token, refresh_token, expires_in }
+    } = await spotifyApi.authorizationCodeGrant(code);
+    res.json({ access_token, refresh_token, expires_in });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+});
+
+app.post('/refresh', async (req, res) => {
+  const { refreshToken } = req.body;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken
+  });
+
+  try {
+    const {
+      body: { access_token, expires_in }
+    } = await spotifyApi.refreshAccessToken();
+    res.json({ access_token, expires_in });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+});
+
+app.listen(PORT, err => {
+  if (err) console.log(err);
+  console.log('Listening on pont ', PORT);
 });
